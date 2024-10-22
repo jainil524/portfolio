88ced9056d323c2c5a3c5f82bea81e531fc96b55
Jainil Prajapati <Jainilprajapati8095@gmail.com>
2024-10-22 22:34:06 +0530

    changed the resume loading from drive URL

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Jainil_Prajapati_Resume_1.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -22,7 +21,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            href="https://drive.google.com/file/d/1nyUKjhSGvmCmfMMIHP6o9i0BuEYotL8X/view?usp=drive_link"
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
